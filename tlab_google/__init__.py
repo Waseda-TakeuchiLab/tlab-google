0d539d68ec431cc6de8f37b9ebd61164e4f57013
Shuhei Nitta <huisintheta@gmail.com>
2022-07-14 03:59:57 +0000

    Update __init__.py

--- tlab_google/__init__.py
+++ tlab_google/__init__.py
@@ -1,12 +1,6 @@
 # Copyright (c) 2022 Shuhei Nitta. All rights reserved.
-from .credentials import Credentials
-from .abstract import AbstractAPI
-from .gmail import GmailAPI
-
 __version__ = "0.0.0"
 
-__all__ = [
-    "Credentials",
-    "AbstractAPI",
-    "GmailAPI"
-]
+from .credentials import Credentials  # noqa
+from .abstract import AbstractAPI  # noqa
+from .gmail import GmailAPI  # noqa
